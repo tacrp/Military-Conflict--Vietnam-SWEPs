9947da77192a636642db486ba9922004f02d842c
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-10-08 09:10:47 -0400

    Update mcv_ak47.lua

--- lua/weapons/mcv_ak47.lua
+++ lua/weapons/mcv_ak47.lua
@@ -33,9 +33,9 @@ SWEP.MuzzleVelocity = 735
 
 SWEP.RangeModifier = 0.950
 
-// "auto", "semi", "burst", "singleaction", "doubleaction", "fanning", "bolt", "pump"
 SWEP.Firemodes = {
-    "auto"
+    MCV.FOREMODE_AUTO,
+    MCV.FOREMODE_SEMI
 }
 
 // Weapon must be manually cycled
