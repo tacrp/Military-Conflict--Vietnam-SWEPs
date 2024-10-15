1a2a0ea457aa6d2f46826f90cc08595cb5d0ff2f
Arctic <mohaodong@gmail.com>
2024-10-15 16:25:03 +1100

    kbkg grenade sound

--- lua/weapons/mcv_kbkg60.lua
+++ lua/weapons/mcv_kbkg60.lua
@@ -130,6 +130,7 @@ SWEP.SoundDoubleShot = ""
 SWEP.SoundReload = ""
 SWEP.SoundSpecial1 = ""
 SWEP.SoundSpecial2 = ""
+SWEP.SoundGrenadeShot = "MCV_Weapon_KBKG60.RifleGrenade"
 SWEP.SoundNearlyEmpty = "Vietnam_Weapon_Generic.NearlyEmptyClick"
 SWEP.SoundEmpty = "Vietnam_Weapon_Generic.ClipEmpty_01"
 
