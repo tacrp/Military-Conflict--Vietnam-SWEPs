7a9e715d8588f2b36bcff561f0bc4c418e60b41e
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-10-15 13:11:40 -0400

    uh oh.............................funny ppoop

--- lua/weapons/mcv_fusil_robust_short.lua
+++ lua/weapons/mcv_fusil_robust_short.lua
@@ -70,7 +70,7 @@ SWEP.ScopeFOV = 8
 SWEP.ScopeFOV2 = 4
 
 SWEP.IronsightPos = Vector(0.05, -2, 0.25)
-SWEP.IronsightAng = Angle(0.2, -0.25, 0)
+SWEP.IronsightAng = Angle(0.29, -0.25, 0)
 
 SWEP.CustomPos = Vector(0, -2, 0)
 SWEP.CustomAng = Angle(0, 0, 0)
