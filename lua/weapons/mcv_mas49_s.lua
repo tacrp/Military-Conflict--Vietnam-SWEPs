8d25c0ae7b2d5c5c54373c8646bd29106da7f8a2
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-10-16 12:34:43 -0400

    hyphens are for losers

--- lua/weapons/mcv_mas49_s.lua
+++ lua/weapons/mcv_mas49_s.lua
@@ -5,7 +5,7 @@ SWEP.Spawnable = true
 AddCSLuaFile()
 
 // Names and basic information
-SWEP.PrintName = "MAS-49 APX L806"
+SWEP.PrintName = "MAS 49 APX L806"
 SWEP.Category = "Military Conflict: Vietnam"
 SWEP.Country = "France"
 SWEP.SubCategory = "Sniper Rifles"
