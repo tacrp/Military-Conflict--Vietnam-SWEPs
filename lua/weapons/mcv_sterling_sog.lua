8e7c23ed555a59f29b766e9b5896e8353cc2a388
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-10-18 16:18:21 -0400

    Update mcv_sterling_sog.lua

--- lua/weapons/mcv_sterling_sog.lua
+++ lua/weapons/mcv_sterling_sog.lua
@@ -142,10 +142,10 @@ SWEP.SoundEmpty = "Vietnam_Weapon_Generic.ClipEmpty_01"
 
 SWEP.MuzzleParticle = "muzzleflash_suppressed"
 SWEP.MuzzleParticleSmoke = ""
-SWEP.MuzzleParticleIronsighted = "muzzleflash_pistol"
+SWEP.MuzzleParticleIronsighted = "muzzleflash_suppressed"
 SWEP.MuzzleParticleIronsightedSmoke = ""
 
-SWEP.MuzzleParticle3rdPerson = "muzzleflash_pistol"
+SWEP.MuzzleParticle3rdPerson = "muzzleflash_suppressed"
 
 SWEP.EjectBrassType = 11
 SWEP.EjectBrassTrail = "shellsmoke"
