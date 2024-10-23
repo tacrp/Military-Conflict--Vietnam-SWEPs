0b90403930f12e9ade3462884b9c9b4990a3c060
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-10-23 13:05:56 -0400

    hdm

---
+++ lua/weapons/mcv_hdm.lua
@@ -0,0 +1,158 @@
+SWEP.Base = "mcv_base"
+
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// Names and basic information
+SWEP.PrintName = "HDM"
+SWEP.Category = "Military Conflict: Vietnam"
+SWEP.Country = "United States of America"
+SWEP.SubCategory = "Pistols"
+SWEP.Caliber = ".22 Long Rifle"
+
+SWEP.Slot = 1
+
+SWEP.ViewModel = "models/weapons/mcv/v_hdm.mdl"
+SWEP.ViewModelAkimbo = "models/weapons/mcv/v_dual_hdm.mdl"
+SWEP.WorldModel = "models/weapons/mcv/w_mle1935.mdl"
+
+SWEP.BodyGroups = ""
+
+SWEP.WeaponSelectIcon = NULL
+
+// Stats
+
+SWEP.DamageGeneric = 30 // damage for other objects (i.e. explosive barrels, breakable walls, or characters with no hitboxes set)
+SWEP.DamageHeadMultiplier = 2.4
+SWEP.DamageChestMultiplier = 1.6
+SWEP.DamageStomachMultiplier = 1.5
+SWEP.DamageLegMultiplier = 1.15
+SWEP.DamageArmMultiplier = 1.15
+
+SWEP.ExplosionDamage = 0
+SWEP.ExplosionRadius = 0
+
+SWEP.MuzzleVelocity = 315
+
+SWEP.RangeModifier = 0.72
+
+// "auto", "semi", "burst", "singleaction", "doubleaction", "fanning", "bolt", "pump"
+SWEP.Firemodes = {
+    MCV.FIREMODE_SEMI
+}
+
+SWEP.LastShotAnimation = true
+
+SWEP.MagInTime = 1
+SWEP.MagInTimeEmpty = 1
+
+// View slide from recoil
+SWEP.ViewSlideRecoilUp = 0.75
+SWEP.ViewSlideRecoilRight = 0.35
+
+SWEP.ViewSlideRecoilIronsightUp = 0.4
+SWEP.ViewSlideRecoilIronsightRight = 0.15
+
+SWEP.RecoilPushbackValue = 1.5
+
+// Camera shake from recoil
+SWEP.ShakeScale = 0.6
+SWEP.ShakeFreq = 60.0
+SWEP.ShakeDuration = 0.30
+
+SWEP.Ironsight = true
+SWEP.IronsightSpeedScale = 0.85
+SWEP.IronsightFov = 90 - 15
+SWEP.IronsightWalkBobbingStrength = -0.25
+
+SWEP.HasScope = false
+SWEP.ScopeMaterial = NULL
+SWEP.ScopeFOV = 8
+SWEP.ScopeFOV2 = 4
+
+SWEP.IronsightPos = Vector(0.11, -4, 0)
+SWEP.IronsightAng = Angle(0.35, 0.05, 0)
+
+SWEP.CustomPos = Vector(0, -2, 0)
+SWEP.CustomAng = Angle(0, 0, 0)
+
+SWEP.Spread = 6.15
+SWEP.SpreadIronsighted = 2.15
+
+SWEP.FireRate = 250 // in rounds per minute -- the MCV script claims its RPM is 50 but there's no way that's correct so im setting it to this for now
+
+SWEP.CrosshairMinDistance = 8
+SWEP.CrosshairDeltaDistance = 4
+
+SWEP.WeaponWeight = 3.85
+
+SWEP.Primary.Ammo = "pistol"
+SWEP.Primary.ClipSize = 10
+SWEP.Primary.Chamber = 1
+SWEP.Primary.DefaultClip = 20
+SWEP.Primary.Automatic = true
+
+SWEP.NearwallDistance = 40
+
+// Bullet spread multiplier according to current stance
+SWEP.CrouchSpreadMultiplier = 0.8
+SWEP.ProneSpreadMultiplier = 0.75
+SWEP.StandMoveSpreadMultiplier = 1.1
+SWEP.SneakMoveSpreadMultiplier = 1
+SWEP.CrouchMoveSpreadMultiplier = 1.05
+SWEP.JumpSpreadMultiplier = 1.25
+
+SWEP.HasBayonet = false
+SWEP.HasBipod = false
+SWEP.HasAkimbo = true
+
+// Penetration
+SWEP.MetalPenetrationDepth = 5
+SWEP.GlassPenetrationDepth = 11
+SWEP.ConcretePenetrationDepth = 7
+SWEP.WoodPenetrationDepth = 15
+SWEP.OtherPenetrationDepth = 9
+
+SWEP.MetalDamageModifier = 1.7
+SWEP.GlassDamageModifier = 1.3
+SWEP.ConcreteDamageModifier = 1.9
+SWEP.WoodDamageModifier = 1.4
+SWEP.OtherDamageModifier = 1.4
+
+// Sound
+SWEP.SoundSingleShot = "MCV_Weapon_HDM.Single"
+SWEP.SoundDoubleShot = ""
+SWEP.SoundReload = ""
+SWEP.SoundSpecial1 = ""
+SWEP.SoundSpecial2 = ""
+SWEP.SoundNearlyEmpty = "MCV_Weapon_Generic.NearlyEmptyClick"
+SWEP.SoundEmpty = "Vietnam_Weapon_Generic.ClipEmpty_01"
+
+// Particles
+// SWEP.MuzzleParticle = "vietnam_muzzleflash_rifle_type1_fp"
+// SWEP.MuzzleParticleSmoke = "vietnam_muzzleflash_rifle_type1_fp_smoke"
+// SWEP.MuzzleParticleIronsighted = "vietnam_muzzleflash_rifle_type1_fp_is"
+// SWEP.MuzzleParticleIronsightedSmoke = "vietnam_muzzleflash_rifle_type1_fp_is_smoke"
+
+// SWEP.MuzzleParticle3rdPerson = "vietnam_muzzleflash_rifle_type1_tp"
+
+// SWEP.EjectBrassType = 1
+// SWEP.EjectBrassTrail = "vietnam_weaponeffect_shelleject_trail"
+// SWEP.EjectBrassParticle = "vietnam_weaponeffect_shelleject_side"
+
+SWEP.MuzzleParticle = "muzzleflash_suppressed"
+SWEP.MuzzleParticleSmoke = ""
+SWEP.MuzzleParticleIronsighted = "muzzleflash_suppressed"
+SWEP.MuzzleParticleIronsightedSmoke = ""
+
+SWEP.MuzzleParticle3rdPerson = "muzzleflash_suppressed"
+
+SWEP.EjectBrassType = 11
+SWEP.EjectBrassTrail = "shellsmoke"
+SWEP.EjectBrassParticle = "port_smoke"
+
+SWEP.TracerParticle = "tracer"
+
+SWEP.TracerRandomness = 6
+SWEP.TracerFrequency = 1
