0f977538cf5527455a567662fbd11690085c85b4
Haodong Mo <mohaodong@gmail.com>
2024-10-23 17:50:10 +1000

    M40 sniper rifle

---
+++ lua/weapons/mcv_m40.lua
@@ -0,0 +1,172 @@
+SWEP.Base = "mcv_base"
+SWEP.Base = "mcv_base"
+
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// Names and basic information
+SWEP.PrintName = "Remington M40"
+SWEP.Category = "Military Conflict: Vietnam"
+SWEP.Country = "United States of America"
+SWEP.SubCategory = "Sniper Rifles"
+SWEP.Caliber = "7.62x51mm"
+
+SWEP.Slot = 3
+
+SWEP.ViewModel = "models/weapons/mcv/v_m40.mdl"
+SWEP.WorldModel = "models/weapons/mcv/w_sks.mdl"
+
+SWEP.BodyGroups = "00000"
+
+SWEP.WeaponSelectIcon = NULL
+
+SWEP.ViewModelFOV = 80
+SWEP.SightedViewModelFOV = 25
+
+// Stats
+
+SWEP.DamageGeneric = 65 // damage for other objects (i.e. explosive barrels, breakable walls, or characters with no hitboxes set)
+SWEP.DamageHeadMultiplier = 2.4
+SWEP.DamageChestMultiplier = 2.15
+SWEP.DamageStomachMultiplier = 1.95
+SWEP.DamageLegMultiplier = 1.15
+SWEP.DamageArmMultiplier = 1.15
+
+SWEP.Num = 1
+
+SWEP.MuzzleVelocity = 735
+
+SWEP.RangeModifier = 0.985 // Every 500 units the damage is multiplied by rangemodifier
+
+SWEP.Firemodes = {
+    MCV.FIREMODE_BOLT
+}
+
+SWEP.LastShotAnimation = false
+SWEP.MagInClip = true
+SWEP.ShotgunReload = true
+SWEP.ShotgunAltReload = true
+SWEP.ShotgunReloadEmptyStartAnimation = false
+SWEP.PlayCycleAnimation = true
+SWEP.HasEmptyReload = false
+
+SWEP.AdjustableScopes = true
+
+// View slide from recoil
+SWEP.ViewSlideRecoilUp = 2.85
+SWEP.ViewSlideRecoilRight = 0.48
+
+SWEP.ViewSlideRecoilIronsightUp = 1.35
+SWEP.ViewSlideRecoilIronsightRight = 0.16
+
+SWEP.RecoilPushbackValue = 1.5
+
+// Camera shake from recoil
+SWEP.ShakeScale = 1
+SWEP.ShakeFreq = 30.0
+SWEP.ShakeDuration = 0.5
+
+SWEP.Ironsight = true
+SWEP.IronsightSpeedScale = 0.95
+SWEP.IronsightFov = 90 - 15
+SWEP.IronsightWalkBobbingStrength = -0.25
+
+SWEP.HasScope = true
+SWEP.ScopeMaterial = Material("models/weapons/mcv/optics/crosshair_4x")
+SWEP.ScopeFOV = 8
+SWEP.ScopeFOV2 = 4
+SWEP.RTScopeMaterialIndex = 2
+
+SWEP.IronsightPos = Vector(0.045, -4, -0.02)
+SWEP.IronsightAng = Angle(0.33, 0.1, 0)
+
+SWEP.CustomPos = Vector(0, -2, 0)
+SWEP.CustomAng = Angle(0, 0, 0)
+
+SWEP.Spread = 7.7
+SWEP.SpreadIronsighted = 0.05
+
+SWEP.FireRate = 60 // in rounds per minute
+SWEP.CyclePostDelay = 0.9
+
+SWEP.CrosshairMinDistance = 8
+SWEP.CrosshairDeltaDistance = 4
+
+SWEP.WeaponWeight = 3.85
+
+SWEP.Primary.Ammo = "ar2"
+SWEP.Primary.ClipSize = 5
+SWEP.Primary.Chamber = 0
+SWEP.Primary.DefaultClip = 15
+SWEP.Primary.Automatic = true
+
+SWEP.NearwallDistance = 40
+
+// Bullet spread multiplier according to current stance
+SWEP.CrouchSpreadMultiplier = 0.65
+SWEP.ProneSpreadMultiplier = 0.4
+SWEP.StandMoveSpreadMultiplier = 1.45
+SWEP.SneakMoveSpreadMultiplier = 1.35
+SWEP.CrouchMoveSpreadMultiplier = 1.25
+SWEP.JumpSpreadMultiplier = 4.0
+
+SWEP.HasBayonet = false
+SWEP.HasRifleGrenade = false
+
+SWEP.BashDamage = 50
+SWEP.BayonetDamage = 100
+
+SWEP.HasBipod = false
+
+// Penetration
+SWEP.MetalPenetrationDepth = 10
+SWEP.GlassPenetrationDepth = 16
+SWEP.ConcretePenetrationDepth = 12
+SWEP.WoodPenetrationDepth = 20
+SWEP.OtherPenetrationDepth = 14
+
+SWEP.MetalDamageModifier = 1.5
+SWEP.GlassDamageModifier = 1.1
+SWEP.ConcreteDamageModifier = 1.7
+SWEP.WoodDamgaeModifier = 1.2
+SWEP.OtherDamageModifier = 1.2
+
+// Sound
+SWEP.SoundSingleShot = "MCV_Weapon_M40.Single"
+SWEP.SoundDoubleShot = ""
+SWEP.SoundReload = ""
+SWEP.SoundSpecial1 = ""
+SWEP.SoundSpecial2 = ""
+SWEP.SoundNearlyEmpty = "MCV_Weapon_Generic.NearlyEmptyClick"
+SWEP.SoundEmpty = "Vietnam_Weapon_Generic.ClipEmpty_01"
+
+// Particles
+// SWEP.MuzzleParticle = "vietnam_muzzleflash_rifle_type1_fp"
+// SWEP.MuzzleParticleSmoke = "vietnam_muzzleflash_rifle_type1_fp_smoke"
+// SWEP.MuzzleParticleIronsighted = "vietnam_muzzleflash_rifle_type1_fp_is"
+// SWEP.MuzzleParticleIronsightedSmoke = "vietnam_muzzleflash_rifle_type1_fp_is_smoke"
+
+// SWEP.MuzzleParticle3rdPerson = "vietnam_muzzleflash_rifle_type1_tp"
+
+// SWEP.EjectBrassType = 1
+// SWEP.EjectBrassTrail = "vietnam_weaponeffect_shelleject_trail"
+// SWEP.EjectBrassParticle = "vietnam_weaponeffect_shelleject_side"
+
+SWEP.MuzzleParticle = "muzzleflash_m24"
+SWEP.MuzzleParticleSmoke = ""
+SWEP.MuzzleParticleIronsighted = "muzzleflash_m24"
+SWEP.MuzzleParticleIronsightedSmoke = ""
+
+SWEP.MuzzleParticle3rdPerson = "muzzleflash_m24"
+
+SWEP.NoEjectOnShoot = true
+
+SWEP.EjectBrassType = 7
+SWEP.EjectBrassTrail = "shellsmoke"
+SWEP.EjectBrassParticle = "port_smoke"
+
+SWEP.TracerParticle = "tracer"
+
+SWEP.TracerRandomness = 6
+SWEP.TracerFrequency = 1
