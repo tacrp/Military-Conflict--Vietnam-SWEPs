3644990c63b0b11f5b3883d4e3268d70c187fc92
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-12-11 11:08:08 -0500

    i think its over

--- lua/weapons/mcv_type64p.lua
+++ lua/weapons/mcv_type64p.lua
@@ -71,8 +71,8 @@ SWEP.ScopeMaterial = NULL
 SWEP.ScopeFOV = 8
 SWEP.ScopeFOV2 = 4
 
-SWEP.IronsightPos = Vector(0.11, -2, 0)
-SWEP.IronsightAng = Angle(0.35, 0.1, 0)
+SWEP.IronsightPos = Vector(0.06, -2, 0)
+SWEP.IronsightAng = Angle(0.2, 0.1, 0)
 
 SWEP.CustomPos = Vector(0, -2, 0)
 SWEP.CustomAng = Angle(0, 0, 0)
